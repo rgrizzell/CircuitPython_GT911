f8764cb02ddf7cc991aa0d954f514d50b2bc826e
Robert Grizzell <robert@grizzell.me>
2023-12-30 10:56:28 -0600

    Simplify method for retrieving touch points.

--- gt911.py
+++ gt911.py
@@ -27,6 +27,7 @@ Implementation Notes
 * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
 * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
 """
+import struct
 import time
 import digitalio
 from micropython import const
@@ -44,30 +45,31 @@ __repo__ = "https://github.com/rgrizzell/CircuitPython_GT911.git"
 _GT_DEFAULT_I2C_ADDR = 0x5D
 _GT_SECONDARY_I2C_ADDR = 0x14
 
+# Commands
 _GT_COMMAND = const(0x8040)
-_GT_COMMAND_CHECK = const(0x8046)
 
-_GT_REG_STATUS = const(0x814E)
-_GT_POINT1_COORD = const(0x814F)
-_GT_POINT2_COORD = const(0x8157)
-_GT_POINT3_COORD = const(0x815F)
-_GT_POINT4_COORD = const(0x8167)
-_GT_POINT5_COORD = const(0x816F)
-_GT_REG_PRODID_1 = const(0x8140)
-_GT_REG_PRODID_2 = const(0x8141)
-_GT_REG_PRODID_3 = const(0x8142)
-_GT_REG_PRODID_4 = const(0x8143)
-_GT_REG_FIRMVERSH = const(0x8145)
-_GT_REG_FIRMVERSL = const(0x8144)
-_GT_REG_VENDID = const(0x814A)
-
-_GT_PANEL_BITFREQH = const(0x8068)
-_GT_PANEL_BITFREQL = const(0x8067)
-_GT_SCREEN_TOUCH_LVL = const(0x8053)
-
-_GT_TOUCH_NO = const(0x804C)
+# Configuration (read-write)
+_GT_CONFIG_START = const(0x8047)
+_GT_X_RESOLUTION = const(0x8048)
+_GT_Y_RESOLUTION = const(0x804A)
+_GT_TOUCH_POINTS = const(0x804C)
+_GT_MODULE_SWITCH1 = const(0x804D)
+_GT_TOUCH_LEVEL = const(0x8053)
+_GT_REFRESH_RATE = const(0x8056)
 _GT_X_THRESHOLD = const(0x8057)
 _GT_Y_THRESHOLD = const(0x8058)
+_GT_FREQUENCY = const(0x8067)
+_GT_CONFIG_CHECKSUM = const(0x80FF)
+_GT_CONFIG_REFRESH = const(0x8100)
+
+# Information (read-only)
+_GT_PRODUCT_ID = const(0x8140)
+_GT_FIRMWARE_VERSION = const(0x8144)
+_GT_VENDOR_ID = const(0x814A)
+
+# Touch Points (read-only)
+_GT_POINT_STATUS = const(0x814E)
+_GT_POINT_START = const(0x814F)
 
 
 class GT911:
@@ -79,8 +81,6 @@ class GT911:
     :type rst_pin: DigitalInOut
     :param int_pin: The object representing the INTERRUPT/IRQ pin.
     :type int_pin: DigitalInOut
-    :param debug: Enables debug output.
-    :type debug: bool
     """
 
     # pylint: disable=too-many-arguments
@@ -91,13 +91,12 @@ class GT911:
         int_pin: digitalio.DigitalInOut = None,
         int_high: bool = False,
         rotation: int = 0,
-        debug: bool = False,
     ):
         self.rst_pin = rst_pin
         self.int_pin = int_pin
         self.int_high = int_high
         self._rotation = rotation
-        self._debug = debug
+        self._touch_data = [tuple()] * 5
 
         # Reset and Interrupt pins are optional, but together they can be used to
         # reset the device into a different I2C configuration.
@@ -106,79 +105,35 @@ class GT911:
         elif self.int_pin:
             # Listen for interrupts
             self.int_pin.switch_to_input()
-
         if self.rst_pin and self.int_pin and self.int_high:
             self._i2c_addr = _GT_DEFAULT_I2C_ADDR
         else:
             self._i2c_addr = _GT_SECONDARY_I2C_ADDR
 
         self.i2c_device = I2CDevice(i2c, self._i2c_addr)
-        # Device initialized
-        if self._debug:
-            print("address:", self._i2c_addr)
-            for key, value in self._device_info().items():
-                print(f"{key}: {value}")
-
-        self._last_touch = self._read_last_touch()
-        self._write(_GT_COMMAND, [0])  # Read coordinates status
+        self._write(_GT_COMMAND, [0])  # Set mode: Read coordinates
 
     @property
-    def touched(self) -> bool:
-        """Check to see if a new touch occurred.
-
-        :return: True if new touch is registered, False if not.
-        :rtype: bool
-        """
-        curr_touch = self._read_last_touch()
-        if self._last_touch != curr_touch:
-            self._last_touch = curr_touch
-            """
-            If this extra call to _read_last_touch()
-            isn't made then the next touch is missed ?????
-            I have no idea why
-            """
-            self._read_last_touch()
-            return True
-        return False
-
-    @property
-    def touches(self) -> list[dict]:
+    def touches(self) -> list[tuple]:
         """Get the touches from the device.
 
-        :return: List of touch points containing an ID and coordinates.
-        :rtype: list[dict]
+        :return: List of touch points containing coordinates and size.
+        :rtype: list[tuple]
         """
-        touch_points = []
-        data = self._last_touch
-        touch_count = 1
-        if self._debug:
-            print(f"touches: {touch_count}")
+        num_touch_points = 0
+        touch_status = self._read(_GT_POINT_STATUS, 1)[0]
+        if touch_status & 0x80:  # if bit7 == 1
+            num_touch_points = touch_status & 0x0F  # get bit0
+            for i in range(0, num_touch_points):
+                data = self._read(_GT_TOUCH_POINTS + i * 8, 8)  # Read the touch point.
+                self._touch_data[i] = struct.unpack(
+                    "hhh", data[1:7]
+                )  # Unpack the coordinates and size.
+            self._write(
+                _GT_POINT_STATUS, [0]
+            )  # Reset the buffer for the next series of touches.
 
-        for _ in range(touch_count):
-            touch_id = data[0]
-
-            x = data[2] * 256 + data[1]
-            y = data[4] * 256 + data[3]
-
-            point = {"id": touch_id, "x": x, "y": y}
-            if self._debug:
-                print(f"id={touch_id} x={x} y={y}")
-            touch_points.append(point)
-        return touch_points
-
-    def _read_last_touch(self) -> list[int]:
-        self._write(_GT_REG_STATUS, [0])
-        test = self._read(_GT_REG_STATUS, 1)[0]
-        timeout = 250
-        while not (test & 0x80) and (timeout := timeout - 1) > 0:
-            if test == 0:
-                break
-            time.sleep(0.001)
-            self._write(_GT_REG_STATUS, [0])
-            test = self._read(_GT_REG_STATUS, 1)[0]
-        self._write(_GT_REG_STATUS, [0])
-
-        return list(self._read(_GT_POINT1_COORD, 7))
+        return self._touch_data[0:num_touch_points]
 
     def _reset_device(self) -> None:
         """If the reset pin is defined, the device can be reset. If the interrupt pin is also
@@ -209,53 +164,40 @@ class GT911:
             self.int_pin.switch_to_input()  # Listen for interrupts
 
     def _read(self, register: int, length: int) -> bytearray:
-        payload = bytes([((register & 0xFF00) >> 8), (register & 0xFF)])
+        payload = bytes([register >> 8, register & 0xFF])
         result = bytearray(length)
-
-        if self.int_pin:
-            while self.int_pin.value:
-                pass
-
         with self.i2c_device as i2c:
             i2c.write_then_readinto(payload, result)
 
-        if self._debug:
-            print("\t$%02X => %s" % (register, [hex(i) for i in result]))
         return result
 
     def _write(self, register: int, values: ReadableBuffer) -> None:
-        values = [((register & 0xFF00) >> 8), (register & 0xFF)] + [
-            (v & 0xFF) for v in values
-        ]
-
+        payload = bytearray([register >> 8, register & 0xFF])
+        payload[2:] = bytes(
+            [(v & 0xFF) for v in values]
+        )  # Ensure each value does not exceed 1 byte.
         with self.i2c_device as i2c:
-            i2c.write(bytes(values))
+            i2c.write(bytes(payload))
 
-        if self._debug:
-            print("\t$%02X <= %s" % (values[0], [hex(i) for i in values[1:]]))
-
-    def _device_info(self):
-        chip_id = chr(self._read(_GT_REG_PRODID_1, 1)[0])
-        chip_id += chr(self._read(_GT_REG_PRODID_2, 1)[0])
-        chip_id += chr(self._read(_GT_REG_PRODID_3, 1)[0])
-        chip_id += chr(self._read(_GT_REG_PRODID_4, 1)[0])
-        firm_id = self._read(_GT_REG_FIRMVERSH, 1)[0]
-        firm_id = (firm_id << 8) | self._read(_GT_REG_FIRMVERSL, 1)[0]
-        vend_id = self._read(_GT_REG_VENDID, 1)[0]
-        num_touch = self._read(_GT_TOUCH_NO, 1)[0] * 0x0F
-        x_thresh = self._read(_GT_X_THRESHOLD, 1)[0]
-        y_thresh = self._read(_GT_Y_THRESHOLD, 1)[0]
-        thresh_lvl = self._read(_GT_SCREEN_TOUCH_LVL, 1)[0]
-        frequency = (self._read(_GT_PANEL_BITFREQH, 1)[0] << 8) | self._read(
-            _GT_PANEL_BITFREQL, 1
-        )[0]
+    def _device_info(self) -> dict:
+        product_id = self._read(_GT_PRODUCT_ID, 4).decode().strip("\x00")
+        firmware_version = struct.unpack("h", self._read(_GT_FIRMWARE_VERSION, 2))[0]
+        vendor_id = self._read(_GT_VENDOR_ID, 1)[0]
+        touch_points = self._read(_GT_TOUCH_POINTS, 1)[0]
+        x_resolution = struct.unpack("h", self._read(_GT_X_RESOLUTION, 2))[0]
+        y_resolution = struct.unpack("h", self._read(_GT_Y_RESOLUTION, 2))[0]
+        x_threshold = self._read(_GT_X_THRESHOLD, 1)[0]
+        y_threshold = self._read(_GT_Y_THRESHOLD, 1)[0]
+        threshold_level = self._read(_GT_TOUCH_LEVEL, 1)[0]
+        refresh_rate = self._read(_GT_REFRESH_RATE, 1)[0]
 
         return {
-            "chip_id": f"{chip_id:4}",
-            "firm_id": f"{firm_id:02X}",
-            "vend_id": f"{vend_id:02X}",
-            "touch_points": num_touch,
-            "thresholds": (x_thresh, y_thresh),
-            "threshold_level": thresh_lvl,
-            "frequency_hz": frequency,
+            "product_id": product_id,
+            "firmware_version": f"{firmware_version:02X}",
+            "vendor_id": f"{vendor_id:02X}",
+            "touch_points": touch_points,
+            "resolution": (x_resolution, y_resolution),
+            "thresholds": (x_threshold, y_threshold),
+            "threshold_level": threshold_level,
+            "refresh_rate": refresh_rate,
         }
