984c64d432d96c92cce349bc0c18172ff8d9a2f7
Robert Grizzell <robert@grizzell.me>
2024-01-08 19:54:57 -0600

    Fix-up build issues

--- gt911.py
+++ gt911.py
@@ -17,8 +17,6 @@ Implementation Notes
 **Hardware:**
 
 * `Product Page <https://www.goodix.com/en/product/touch/touch_screen_controller>`_
-* `GT911 Datasheet
- <https://www.lcd-module.de/fileadmin/eng/pdf/zubehoer/GT911%20Programming%20Guide_20140804_Rev00.pdf>`_
 
 **Software and Dependencies:**
 
@@ -45,29 +43,7 @@ __repo__ = "https://github.com/rgrizzell/CircuitPython_GT911.git"
 _GT_DEFAULT_I2C_ADDR = 0x5D
 _GT_SECONDARY_I2C_ADDR = 0x14
 
-# Commands
 _GT_COMMAND = const(0x8040)
-
-# Configuration (read-write)
-_GT_CONFIG_START = const(0x8047)
-_GT_X_RESOLUTION = const(0x8048)
-_GT_Y_RESOLUTION = const(0x804A)
-_GT_TOUCH_POINTS = const(0x804C)
-_GT_MODULE_SWITCH1 = const(0x804D)
-_GT_TOUCH_LEVEL = const(0x8053)
-_GT_REFRESH_RATE = const(0x8056)
-_GT_X_THRESHOLD = const(0x8057)
-_GT_Y_THRESHOLD = const(0x8058)
-_GT_FREQUENCY = const(0x8067)
-_GT_CONFIG_CHECKSUM = const(0x80FF)
-_GT_CONFIG_REFRESH = const(0x8100)
-
-# Information (read-only)
-_GT_PRODUCT_ID = const(0x8140)
-_GT_FIRMWARE_VERSION = const(0x8144)
-_GT_VENDOR_ID = const(0x814A)
-
-# Touch Points (read-only)
 _GT_POINT_STATUS = const(0x814E)
 _GT_POINT_START = const(0x814F)
 
@@ -90,18 +66,16 @@ class GT911:
         rst_pin: digitalio.DigitalInOut = None,
         int_pin: digitalio.DigitalInOut = None,
         int_high: bool = False,
-        rotation: int = 0,
     ):
         self.rst_pin = rst_pin
         self.int_pin = int_pin
         self.int_high = int_high
-        self._rotation = rotation
         self._touch_data = [tuple()] * 5
 
         # Reset and Interrupt pins are optional, but together they can be used to
         # reset the device into a different I2C configuration.
         if self.rst_pin:
-            self._reset_device()
+            self._reset()
         elif self.int_pin:
             # Listen for interrupts
             self.int_pin.switch_to_input()
@@ -125,7 +99,7 @@ class GT911:
         if touch_status & 0x80:  # if bit7 == 1
             num_touch_points = touch_status & 0x0F  # get bit0
             for i in range(0, num_touch_points):
-                data = self._read(_GT_TOUCH_POINTS + i * 8, 8)  # Read the touch point.
+                data = self._read(_GT_POINT_START + i * 8, 8)  # Read the touch point.
                 self._touch_data[i] = struct.unpack(
                     "hhh", data[1:7]
                 )  # Unpack the coordinates and size.
@@ -135,7 +109,7 @@ class GT911:
 
         return self._touch_data[0:num_touch_points]
 
-    def _reset_device(self) -> None:
+    def _reset(self) -> None:
         """If the reset pin is defined, the device can be reset. If the interrupt pin is also
         defined, device can be reset into a specific I2C address configuration.
         """
@@ -178,26 +152,3 @@ class GT911:
         )  # Ensure each value does not exceed 1 byte.
         with self.i2c_device as i2c:
             i2c.write(bytes(payload))
-
-    def _device_info(self) -> dict:
-        product_id = self._read(_GT_PRODUCT_ID, 4).decode().strip("\x00")
-        firmware_version = struct.unpack("h", self._read(_GT_FIRMWARE_VERSION, 2))[0]
-        vendor_id = self._read(_GT_VENDOR_ID, 1)[0]
-        touch_points = self._read(_GT_TOUCH_POINTS, 1)[0]
-        x_resolution = struct.unpack("h", self._read(_GT_X_RESOLUTION, 2))[0]
-        y_resolution = struct.unpack("h", self._read(_GT_Y_RESOLUTION, 2))[0]
-        x_threshold = self._read(_GT_X_THRESHOLD, 1)[0]
-        y_threshold = self._read(_GT_Y_THRESHOLD, 1)[0]
-        threshold_level = self._read(_GT_TOUCH_LEVEL, 1)[0]
-        refresh_rate = self._read(_GT_REFRESH_RATE, 1)[0]
-
-        return {
-            "product_id": product_id,
-            "firmware_version": f"{firmware_version:02X}",
-            "vendor_id": f"{vendor_id:02X}",
-            "touch_points": touch_points,
-            "resolution": (x_resolution, y_resolution),
-            "thresholds": (x_threshold, y_threshold),
-            "threshold_level": threshold_level,
-            "refresh_rate": refresh_rate,
-        }
